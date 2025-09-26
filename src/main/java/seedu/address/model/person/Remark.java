382e470206b8366865d80084cbbc261522444179
Glenn <glennchiang24@gmail.com>
2025-09-26 11:53:47 +0800

    Remove remark validation

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -26,15 +26,7 @@ public class Remark {
      */
     public Remark(String remark) {
         requireNonNull(remark);
-        checkArgument(isValidRemark(remark), MESSAGE_CONSTRAINTS);
         value = remark;
-    }
-
-    /**
-     * Returns true if a given string is a valid email.
-     */
-    public static boolean isValidRemark(String test) {
-        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
