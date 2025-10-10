b26b788b97bf03c116e037f86e26575ee386a380
Whyudothiss <weichenhow@gmail.com>
2025-10-10 12:32:49 +0800

    Update RemoveSubComMemberCommandParser

--- src/main/java/seedu/excolink/logic/parser/RemoveSubcomMemberCommandParser.java
+++ src/main/java/seedu/excolink/logic/parser/RemoveSubcomMemberCommandParser.java
@@ -1,7 +1,10 @@
 package seedu.excolink.logic.parser;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.excolink.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.excolink.logic.parser.CliSyntax.PREFIX_SUBCOM;
 
+import seedu.excolink.commons.core.index.Index;
 import seedu.excolink.logic.commands.RemoveSubcomMemberCommand;
 import seedu.excolink.logic.parser.exceptions.ParseException;
 
@@ -11,6 +14,25 @@ import seedu.excolink.logic.parser.exceptions.ParseException;
 public class RemoveSubcomMemberCommandParser implements Parser<RemoveSubcomMemberCommand>{
     public RemoveSubcomMemberCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_SUBCOM);
+
+        String preamble = argMultimap.getPreamble();
+        if (preamble.isEmpty() || !argMultimap.getValue(PREFIX_SUBCOM).isPresent()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveSubcomMemberCommand.MESSAGE_USAGE));
+        }
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(preamble);
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemoveSubcomMemberCommand.MESSAGE_USAGE, pe));
+        }
+
+        String subcomName = argMultimap.getValue(PREFIX_SUBCOM).get().trim();
+        if (subcomName.isEmpty()) {
+            throw new ParseException("Subcommittee name cannot be empty");
+        }
+
+        return new RemoveSubcomMemberCommand(index, subcomName);
     }
 }
