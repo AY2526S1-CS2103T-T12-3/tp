6a4651a0e7f41ccc08e823901fab5c18be22893e
Glenn <glennchiang24@gmail.com>
2025-10-13 23:08:10 +0800

    Update sample data with sample subcoms

--- src/main/java/seedu/excolink/model/util/SampleDataUtil.java
+++ src/main/java/seedu/excolink/model/util/SampleDataUtil.java
@@ -18,45 +18,48 @@ import seedu.excolink.model.subcom.Subcom;
  * Contains utility methods for populating {@code ExcoLink} with sample data.
  */
 public class SampleDataUtil {
-    private static Subcom publicity = new Subcom("Publicity");
-    private static Subcom logistics = new Subcom("Logistics");
-    private static Subcom relations = new Subcom("relations");
+    private static Subcom tech = new Subcom("Tech");
+    private static Subcom uiUx = new Subcom("UI/UX");
+    private static Subcom brandingAndMarketing = new Subcom("Branding & Marketing");
 
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
                     new Address("Blk 30 Geylang Street 29, #06-40"),
                     getRoleSet("friends"),
-                    publicity),
+                    tech),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                     new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
                     getRoleSet("colleagues", "friends"),
-                    logistics),
+                    uiUx),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                     new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
                     getRoleSet("neighbours"),
-                    relations),
+                    brandingAndMarketing),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                     new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
                     getRoleSet("family"),
-                    publicity),
+                    tech),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                     new Address("Blk 47 Tampines Street 20, #17-35"),
                     getRoleSet("classmates"),
-                    logistics),
+                    uiUx),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                     new Address("Blk 45 Aljunied Street 85, #11-31"),
                     getRoleSet("colleagues"),
-                    relations)
+                    brandingAndMarketing)
         };
     }
 
     public static ReadOnlyExcoLink getSampleExcoLink() {
-        ExcoLink sampleAb = new ExcoLink();
+        ExcoLink sampleExcoLink = new ExcoLink();
         for (Person samplePerson : getSamplePersons()) {
-            sampleAb.addPerson(samplePerson);
+            sampleExcoLink.addPerson(samplePerson);
         }
-        return sampleAb;
+        sampleExcoLink.addSubcom(tech);
+        sampleExcoLink.addSubcom(uiUx);
+        sampleExcoLink.addSubcom(brandingAndMarketing);
+        return sampleExcoLink;
     }
 
     /**
