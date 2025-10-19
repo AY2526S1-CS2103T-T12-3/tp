86a1058a10119e0bf273c65c6dfa6f5625457a62
Whyudothiss <weichenhow@gmail.com>
2025-10-20 01:53:02 +0800

    Remove unused import statements

--- src/test/java/seedu/excolink/logic/parser/UnassignSubcomCommandParserTest.java
+++ src/test/java/seedu/excolink/logic/parser/UnassignSubcomCommandParserTest.java
@@ -1,8 +1,5 @@
 package seedu.excolink.logic.parser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static seedu.excolink.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.excolink.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.excolink.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.excolink.testutil.TypicalIndexes.INDEX_SECOND;
@@ -10,9 +7,7 @@ import static seedu.excolink.testutil.TypicalIndexes.INDEX_SECOND;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import seedu.excolink.commons.core.index.Index;
 import seedu.excolink.logic.commands.UnassignSubcomCommand;
-import seedu.excolink.logic.parser.exceptions.ParseException;
 
 public class UnassignSubcomCommandParserTest {
     private UnassignSubcomCommandParser parser;
