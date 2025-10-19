984b0b4172b6a4e297fbd182b48b0a6a29d4ebbf
Whyudothiss <weichenhow@gmail.com>
2025-10-20 01:52:03 +0800

    Refractor RemoveSubcomMemberCommandParser to UnassignSubcomCommandParser

--- src/main/java/seedu/excolink/logic/parser/UnassignSubcomCommandParser.java
+++ src/main/java/seedu/excolink/logic/parser/UnassignSubcomCommandParser.java
@@ -5,28 +5,28 @@ import static seedu.excolink.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.excolink.logic.parser.CliSyntax.PREFIX_SUBCOM;
 
 import seedu.excolink.commons.core.index.Index;
-import seedu.excolink.logic.commands.RemoveSubcomMemberCommand;
+import seedu.excolink.logic.commands.UnassignSubcomCommand;
 import seedu.excolink.logic.parser.exceptions.ParseException;
 
 /**
  * Parses input arguments and creates a new RemoveSubcomMemberCommand object
  */
-public class RemoveSubcomMemberCommandParser implements Parser<RemoveSubcomMemberCommand> {
+public class UnassignSubcomCommandParser implements Parser<UnassignSubcomCommand> {
 
     /**
-     * Parses the given string of arguments in the context of the RemoveSubcomMemberCommand
+     * Parses the given string of arguments in the context of the UnassignSubcomcCommand
      * @param args given string of arguments
-     * @return a RemoveSubcomMemberCommand object
+     * @return an UnassignSubcomCommand object
      * @throws ParseException if the user input does not follow the required format
      */
-    public RemoveSubcomMemberCommand parse(String args) throws ParseException {
+    /**public UnassignSubcomCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_SUBCOM);
 
         String preamble = argMultimap.getPreamble();
         if (preamble.isEmpty() || !argMultimap.getValue(PREFIX_SUBCOM).isPresent()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    RemoveSubcomMemberCommand.MESSAGE_USAGE));
+                    UnassignSubcomCommand.MESSAGE_USAGE));
         }
 
         Index index;
@@ -34,7 +34,7 @@ public class RemoveSubcomMemberCommandParser implements Parser<RemoveSubcomMembe
             index = ParserUtil.parseIndex(preamble);
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    RemoveSubcomMemberCommand.MESSAGE_USAGE, pe));
+                    UnassignSubcomCommand.MESSAGE_USAGE, pe));
         }
 
         String subcomName = argMultimap.getValue(PREFIX_SUBCOM).get().trim();
@@ -42,6 +42,16 @@ public class RemoveSubcomMemberCommandParser implements Parser<RemoveSubcomMembe
             throw new ParseException("Subcommittee name cannot be empty");
         }
 
-        return new RemoveSubcomMemberCommand(index, subcomName);
+        return new UnassignSubcomCommand(index, subcomName);
+    }**/
+    @Override
+    public UnassignSubcomCommand parse(String args) throws ParseException {
+        try {
+            Index index = ParserUtil.parseIndex(args.trim());
+            return new UnassignSubcomCommand(index);
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnassignSubcomCommand.MESSAGE_USAGE), pe);
+        }
     }
 }
