e88f0902c476a51309a66fb4c1b02bd918dee0a0
Sengernest <sengernest69@gmail.com>
2025-11-03 20:18:33 +0800

    Fix checkstyle issues

--- src/main/java/seedu/excolink/ui/PersonCard.java
+++ src/main/java/seedu/excolink/ui/PersonCard.java
@@ -48,13 +48,13 @@ public class PersonCard extends UiPart<Region> {
     public PersonCard(Person person, int displayedIndex) {
         super(FXML);
         this.person = person;
-
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
         email.setText(person.getEmail().value);
         subcom.setText(person.getSubcom().value);
-        subcom.setStyle(person.getSubcom().equals(Subcom.NOSUBCOM) ? "-fx-font-weight: normal;" : ("-fx-font-weight: bold;"));
+        subcom.setStyle(person.getSubcom().equals(Subcom.NOSUBCOM) ? "-fx-font-weight: normal;"
+                                                                   : ("-fx-font-weight: bold;"));
 
         person.getRoles().stream()
                 .sorted(Comparator.comparing(role -> role.roleName))
